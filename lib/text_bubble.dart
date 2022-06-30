1a1955309000a209f4893a5dbe11d3166f1df5dd
KeysGilbert <gbk5070@psu.edu>
2022-06-30 12:57:12 -0400

    change bubble color for different users

--- lib/text_bubble.dart
+++ lib/text_bubble.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class textBubble extends StatelessWidget {
   final String text;
   final String sender;
-  textBubble({this.text, this.sender});
+  final bool isMe;
+  textBubble({this.text, this.sender, this.isMe});
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +18,20 @@ class textBubble extends StatelessWidget {
             style: TextStyle(fontSize: 10.0, color: Colors.black45),
           ),
           Material(
-              elevation: 5.0,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30)),
-              color: Colors.lightBlueAccent,
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                child: Text(
-                  '$text',
-                  style: TextStyle(color: Colors.white),
-                ),
-              )),
+            elevation: 5.0,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30)),
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                '$text',
+                style: TextStyle(color: isMe ? Colors.white : Colors.black54),
+              ),
+            ),
+          ),
         ],
       ),
     );
